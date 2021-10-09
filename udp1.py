f60fc790021fe5cd134716d7b6f573c2f0c8c436
15388743657 <90780590+15388743657@users.noreply.github.com>
2021-10-09 09:38:11 +0800

    Update udp1.py

--- udp1.py
+++ udp1.py
@@ -4,10 +4,10 @@ s.bind(('127.0.0.1',9999))
 print('Bind UDP on 8088...')
 while True:
       #接收数据                        
-      adddr,str1=s.recvfrom(1024)
+      addr,str1=s.recvfrom(1024)
       print('Received from %s:%s.'%str1)
       str2=str(str1,encoding='utf-8')
       print('I recieve a string is: ',str2)
       str3=str2.upper()
-      s.sendto(reply.encode('utf-8'),str1)
+      s.sendto(str3.encode('utf-8'),str1)
 s.close()
