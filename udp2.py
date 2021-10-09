70578268c19a086ba964d2cc8777a1681e1faf6f
15388743657 <90780590+15388743657@users.noreply.github.com>
2021-10-09 09:40:24 +0800

    Update udp2.py

--- udp2.py
+++ udp2.py
@@ -4,7 +4,7 @@ s.connect(('127.0.0.1',9999))
 print('I am connecting the Server!')
 for fwt in ['aBch','f服务d','h7Tq','.']:
     s.sendto(fwt.encode('utf-8'),('10.128.67.150',9999))
-    addr,str1=s.recvfrom(1024)
+    str1,addr=s.recvfrom(1024)
     str2=str(str1,encoding='utf-8')
     print('The original string is :',fwt,'\tthe processed string is :',str2)
 s.close()
