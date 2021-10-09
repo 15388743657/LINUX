6ec9e92354a447b13a98ff19147350e1f32b4ea1
15388743657 <90780590+15388743657@users.noreply.github.com>
2021-10-09 09:04:01 +0800

    Add files via upload

---
+++ udp1.py
@@ -0,0 +1,13 @@
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('127.0.0.1',9999))
+print('Bind UDP on 8088...')
+while True:
+      #接收数据                        
+      adddr,str1=s.recvfrom(1024)
+      print('Received from %s:%s.'%str1)
+      str2=str(str1,encoding='utf-8')
+      print('I recieve a string is: ',str2)
+      str3=str2.upper()
+      s.sendto(reply.encode('utf-8'),str1)
+s.close()
